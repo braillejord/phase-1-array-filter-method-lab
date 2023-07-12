8ce3469773d80880802ceeaa9a3fab903efd81d0
BreElle Wells <breellejordyn@gmail.com>
2023-07-12 14:01:26 -0600

    practice using filter()

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(collection, name) {
+    return collection.filter(
+        (nameMatch) => nameMatch.toLowerCase() === name.toLowerCase()
+    );
+}
+
+function fuzzyMatch(collection, name) {
+    return collection.filter(
+        (firstLetter) => firstLetter.charAt(0) === name.charAt(0)
+    );
+}
+
+function matchName(collection, name) {
+    return collection.filter(
+        (nameMatch) => nameMatch.name === name
+    );
+}
